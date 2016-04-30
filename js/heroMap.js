2a5a8d34f979643b4a0fc8676602548351e8beba
cwmat <chaz.mateer@gmail.com>
2016-04-30 16:39:44 -0400

    Added navbar that links directly to vis.

--- js/heroMap.js
+++ js/heroMap.js
@@ -6,8 +6,6 @@
 
 HeroMap = function(parentElement) {
   this.parentElement = parentElement;
-  // this.data = _data;
-  // this.center = [41.581168, -96.599876];
   this.center = [39.277449, -97.632677];
   this.zoomLevel = 4;
   this.zooms = [
@@ -60,11 +58,10 @@ HeroMap.prototype.initVis = function() {
   L.Icon.Default.imagePath = 'img';
 
   vis.map = L.map(vis.parentElement).setView(vis.center, vis.zoomLevel);
-  // vis.basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contrib utors' }).addTo(vis.map);
 
   vis.basemap = L.esri.basemapLayer('Imagery').addTo(vis.map)
 
-  vis.parks = L.esri.dynamicMapLayer({
+  vis.turbines = L.esri.dynamicMapLayer({
         url: "http://eerscmap.usgs.gov/arcgis/rest/services/wind/wTurbinesWM/MapServer",
         style: function () {
           return { color: "#70ca49", weight: 2 };
@@ -75,81 +72,10 @@ HeroMap.prototype.initVis = function() {
 
   function panMap() {
     var rando = Math.floor(Math.random() * vis.zooms.length);
-    console.log(rando);
     setTimeout(function() {
       var zoom = vis.zooms[rando];
       vis.map.setView(zoom.coords, zoom.zoom, {animate: true, duration: 2})
       panMap();
     }, 10000);
   }
-
-
-  // Layers
-  // vis.lines = L.layerGroup().addTo(vis.map);
-  // vis.stations = L.layerGroup().addTo(vis.map);
-  //
-  // // Load MBTA lines
-  // function lineStyle(feature, layer) {
-  //   return {color: feature.properties.LINE};
-  // }
-  // function linePopup(feature, layer) {
-  //   layer.bindPopup("Route: " + feature.properties.ROUTE);
-  // }
-  // $.getJSON("../data/MBTA-Lines.json", function(data) {
-  //   vis.lines.addLayer(L.geoJson(data, {
-  //     style: lineStyle,
-  //     onEachFeature: linePopup
-  //   }));
-  // });
-  //
-  // vis.data.forEach(function(station) {
-  //   var latlng = [station.lat, station.long];
-  //   var popupContent = "Station: " + station.name + "<br>" +
-  //                       "Bikes: " + station.nbBikes + "<br>" +
-  //                       "Docks: " + station.nbEmptyDocks;
-  //
-  //   vis.stations.addLayer(L.marker(latlng).bindPopup(popupContent));
-  // });
-
-  vis.wrangleData();
-}
-
-
-/*
- *  Data wrangling
- */
-
-HeroMap.prototype.wrangleData = function() {
-  var vis = this;
-
-  // Currently no data wrangling/filtering needed
-  // vis.displayData = vis.data;
-
-  // Update the visualization
-  vis.updateVis();
-}
-
-
-/*
- *  The drawing function
- */
-
-HeroMap.prototype.updateVis = function() {
-  var vis = this;
-  //
-  // vis.lines = L.layerGroup().addTo(vis.map);
-  // vis.stations = L.layerGroup().addTo(vis.map);
-  //
-  // vis.lines.addLayer(L.geoJSON);
-  //
-  // vis.data.forEach(function(station) {
-  //   var latlng = [station.lat, station.long];
-  //   var popupContent = "Station: " + station.name + "<br>" +
-  //                       "Bikes: " + station.nbBikes + "<br>" +
-  //                       "Docks: " + station.nbEmptyDocks;
-  //
-  //   vis.stations.addLayer(L.marker(latlng).bindPopup(popupContent));
-  // });
-
-
 }
